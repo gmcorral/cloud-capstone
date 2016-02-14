15e39855dee3b4ea1de70a5bc7b8b347486b667f
Guillermo Menendez <gmcorral@gmail.com>
2016-02-14 16:48:30 +0100

    Fixed g1ex3

--- spark/g1e3.py
+++ spark/g1e3.py
@@ -31,9 +31,9 @@ def signal_handler(signal, frame):
 
 def print_rdd(rdd):
     print('=============================')
-    daysOfWeek = rdd.takeOrdered(10, key = lambda x: -x[1])
+    daysOfWeek = rdd.takeOrdered(10, key = lambda x: x[1][0]/x[1][1])
     for day in daysOfWeek:
-        print(day)
+        print('(' + str(day[0]) + ', ' + str(day[1][0] / day[1][1]) + ')')
     print('=============================')
 
 ######
@@ -41,7 +41,19 @@ def print_rdd(rdd):
 ######
 
 def updateFunction(new_values, last_sum):
-    return sum(new_values) + (last_sum or 0)
+
+    new_vals0 = 0.0
+    new_vals1 = 0
+    
+    for val in new_values:
+        new_vals0 += val[0]
+        new_vals1 += val[1]
+    
+    last_vals0 = last_sum[0] if last_sum is not None else 0.0
+    last_vals1 = last_sum[1] if last_sum is not None else 0
+    
+    return (new_vals0 + last_vals0,\
+            new_vals1 + last_vals1) 
 
 ######
 ###### Main script #######
@@ -51,6 +63,7 @@ signal.signal(signal.SIGINT, signal_handler)
 
 config = SparkConf()
 config.set('spark.streaming.stopGracefullyOnShutdown', True)
+config.set('spark.yarn.executor.memoryOverhead', '2g')
 
 sc = SparkContext(appName='g1ex3', conf=config, pyFiles=['flight.py'])
 ssc = StreamingContext(sc, 1)
@@ -60,7 +73,7 @@ lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
 
 filtered = lines.map(lambda line: line.split(","))\
                 .map(lambda fields: Flight(fields))\
-                .map(lambda fl: (fl.DayOfWeek, 1))\
+                .map(lambda fl: (fl.DayOfWeek, (fl.ArrDelay, 1)))\
                 .updateStateByKey(updateFunction)
 
 filtered.foreachRDD(lambda rdd: print_rdd(rdd))
