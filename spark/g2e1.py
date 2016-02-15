0af9f73c6c35207d7d94c7c29ebed13eae34ac78
Guillermo Menendez <gmcorral@gmail.com>
2016-02-15 22:46:14 +0100

    Added Cancelled condition on flights

--- spark/g2e1.py
+++ spark/g2e1.py
@@ -31,9 +31,9 @@ def signal_handler(signal, frame):
 
 def print_rdd(rdd):
     print('=============================')
-    daysOfWeek = rdd.takeOrdered(10, key = lambda x: x[1][0]/x[1][1])
+    daysOfWeek = rdd.takeOrdered(10, key = lambda x: -x[1])
     for day in daysOfWeek:
-        print('(' + str(day[0]) + ', ' + str(day[1][0] / day[1][1]) + ')')
+        print(day)
     print('=============================')
 
 ######
@@ -41,19 +41,7 @@ def print_rdd(rdd):
 ######
 
 def updateFunction(new_values, last_sum):
-
-    new_vals0 = 0.0
-    new_vals1 = 0
-    
-    for val in new_values:
-        new_vals0 += val[0]
-        new_vals1 += val[1]
-    
-    last_vals0 = last_sum[0] if last_sum is not None else 0.0
-    last_vals1 = last_sum[1] if last_sum is not None else 0
-    
-    return (new_vals0 + last_vals0,\
-            new_vals1 + last_vals1) 
+    return sum(new_values) + (last_sum or 0)
 
 ######
 ###### Main script #######
@@ -63,18 +51,16 @@ signal.signal(signal.SIGINT, signal_handler)
 
 config = SparkConf()
 config.set('spark.streaming.stopGracefullyOnShutdown', True)
-config.set('spark.yarn.executor.memoryOverhead', '2g')
 
-sc = SparkContext(appName='g1ex3', conf=config, pyFiles=['flight.py'])
+sc = SparkContext(appName='g2ex1', conf=config, pyFiles=['flight.py'])
 ssc = StreamingContext(sc, 1)
-ssc.checkpoint('/tmp/g1ex3')
+ssc.checkpoint('/tmp/g2ex1')
 
 lines = ssc.socketTextStream(sys.argv[1], int(sys.argv[2]))
 
 filtered = lines.map(lambda line: line.split(","))\
                 .map(lambda fields: Flight(fields))\
-                .filter(lambda fl: fl.Cancelled == 0)\
-                .map(lambda fl: (fl.DayOfWeek, (fl.ArrDelay, 1)))\
+                .map(lambda fl: (fl.DayOfWeek, 1))\
                 .updateStateByKey(updateFunction)
 
 filtered.foreachRDD(lambda rdd: print_rdd(rdd))
